382e24b876b26bbfab4e5f7564db6b74504aa015
debabratamana <allrounderdebu@gmail.com>
2026-07-09 09:48:04 +0530

    Time: 1 ms (44.63%), Space: 50.6 MB (14.4%) - LeetHub

---
+++ 3668-restore-finishing-order/3668-restore-finishing-order.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
+        unordered_set<int> st(friends.begin(), friends.end());
+
+        vector<int> ans;
+
+        for (int id : order) {
+            if (st.count(id)) {
+                ans.push_back(id);
+            }
+        }
+
+        return ans;
+    }
+};
